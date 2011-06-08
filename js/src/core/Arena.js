b6e203713bb02b402c3a204cdeefd23a5d40b30d
hayfield <hayfield@gmx.com>
2011-06-08 12:56:31 +0100

    Add an init function

--- js/src/core/Arena.js
+++ js/src/core/Arena.js
@@ -1,5 +1,21 @@
 SRJS.Arena = function( args ){
 
-	this.scene = typeof args === 'undefined' ? new THREE.Scene() : args.scene || new THREE.Scene();
+	if( typeof args === 'undefined' ){
+		
+		this.scene = new THREE.Scene();
+		
+		this.init = function(){
+			console.log('no arguments passed to SRJS.Arena()');
+		};
+		
+	} else {
+	
+		this.scene = args.scene || new THREE.Scene();
+		
+		this.init = args.init || function(){
+			console.log('incomplete arguments passed to SRJS.Arena() - missing init');
+		};
+	
+	}
 	
 };
