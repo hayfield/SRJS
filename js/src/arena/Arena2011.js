0af91507226d73c447de956e4d393b0ad53ea278
hayfield <hayfield@gmx.com>
2011-06-09 11:35:13 +0100

    Add lights

--- js/src/arena/Arena2011.js
+++ js/src/arena/Arena2011.js
@@ -3,7 +3,7 @@ SRJS.Arena2011 = function(){
 	args = {};
 	
 	var camera, scene, renderer,
-    geometry, material, mesh, stats;
+    geometry, material, mesh, stats, light;
 
 	args.initScene = function(){
 		//camera = new THREE.Camera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
@@ -14,9 +14,23 @@ SRJS.Arena2011 = function(){
 					movementSpeed: 1000, lookSpeed: 0.125, noFly: false, lookVertical: true, autoForward: false
  
 				} );
-        camera.position.z = 1000;
+        camera.position.y = 100;
 
         scene = new THREE.Scene();
+		
+		var ambient = new THREE.AmbientLight( 0xffffff );
+		scene.addLight( ambient );
+ 
+		var directionalLight = new THREE.DirectionalLight( 0xff0000 );
+		directionalLight.position.y = 200;
+		directionalLight.position.normalize();
+		scene.addLight( directionalLight );
+ 
+		var pointLight = new THREE.PointLight( 0xff0000 );
+		pointLight.position.y = 150;
+		pointLight.distance = 100000;
+		pointLight.intensity = 0.5;
+		scene.addLight( pointLight );
 
         geometry = new THREE.Cube( 200, 200, 200 );
         material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
@@ -64,7 +78,7 @@ SRJS.Arena2011 = function(){
         mesh = new THREE.Mesh( geometry, material );
         scene.addObject( mesh );
 
-        renderer = new THREE.CanvasRenderer();
+        renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
 
         document.body.appendChild( renderer.domElement );
--- js/src/arena/Material.js
+++ js/src/arena/Material.js
@@ -1,8 +1,8 @@
 SRJS.Material = {
 	
-	white: new THREE.MeshBasicMaterial( { color: 0xffffff } ),
-	red: new THREE.MeshBasicMaterial( { color: 0xff0000 } ),
-	green: new THREE.MeshBasicMaterial( { color: 0x00ff00 } ),
-	blue: new THREE.MeshBasicMaterial( { color: 0x0000ff } )
+	white: new THREE.MeshPhongMaterial( { color: 0xffffff } ),
+	red: new THREE.MeshPhongMaterial( { color: 0xff0000 } ),
+	green: new THREE.MeshPhongMaterial( { color: 0x00ff00 } ),
+	blue: new THREE.MeshPhongMaterial( { color: 0x0000ff } )
 	
 };
