0438444536ec08833f79072c971c61f8da038cd0
hayfield <hayfield@gmx.com>
2011-06-30 14:55:35 +0100

    Allow parameters to be passed when creating a Blob

--- js/src/vision/Blob.js
+++ js/src/vision/Blob.js
@@ -1,9 +1,9 @@
-SRJS.Blob = function(){
+SRJS.Blob = function( x, y, width, height ){
 
-	this.x = 0;
-	this.y = 0;
-	this.width = 0;
-	this.height = 0;
+	this.x = x || 0;
+	this.y = y || 0;
+	this.width = width || 0;
+	this.height = height || 0;
 
 };
 
