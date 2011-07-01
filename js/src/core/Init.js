5308ee8ccbee78535096d64b1511a28338cd18c6
hayfield <hayfield@gmx.com>
2011-07-01 17:45:39 +0100

    Add several more robots so there are 4 in at a time

--- js/src/core/Init.js
+++ js/src/core/Init.js
@@ -5,5 +5,8 @@ SRJS.Init = function(){
 	var robby = new SRJS.Robot();
 	robby.motor[0].target = 30;
 	SRJS.CURRENT_ARENA.addRobot( robby );
-	
+	SRJS.CURRENT_ARENA.addRobot();
+	SRJS.CURRENT_ARENA.robots[2].motor[0].target = 35;
+	SRJS.CURRENT_ARENA.addRobot();
+	SRJS.CURRENT_ARENA.robots[3].motor[0].target = 45;
 };
