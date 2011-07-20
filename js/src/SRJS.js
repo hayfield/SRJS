d74f46115a8a4a2e331268da8f897bc8550bb04d
hayfield <hayfield@gmx.com>
2011-07-20 17:49:25 +0100

    Increase the size of the physics canvas

--- js/src/SRJS.js
+++ js/src/SRJS.js
@@ -2,5 +2,5 @@ var SRJS = SRJS || {};
 
 SRJS.floatyCam = false;
 SRJS.rendererDimension = 200; // window.innerWidth, window.innerHeight
-SRJS.physicsDimension = 400; // the dimension of the physics canvas
+SRJS.physicsDimension = 800; // the dimension of the physics canvas
 SRJS.displayStats = false;
