13fd646b9e4ba5daad0267d82374336d3401cef1
hayfield <hayfield@gmx.com>
2011-07-20 15:53:35 +0100

    Clear the physics canvas before each pass

--- js/src/physics/Environment.js
+++ js/src/physics/Environment.js
@@ -16,26 +16,25 @@ SRJS.Physics.Environment = function(){
 		if( count < 5 ){
 			console.log('drawing', ctx, this.polygons[0].edges);
 		}
+		
+		ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height );
 		var p, e, polygon, edge;
 		p = 0;
 		//ctx.clearRect( 0, 0, 800, 800 );
 		while( p < this.polygons.length ){
 			polygon = this.polygons[p];			
-
 			e = 0;
 			while( e < polygon.edges.length ){
 				edge = polygon.edges[e];
+				
 				ctx.beginPath();
 				ctx.strokeStyle = colors[ e % 4 ];
 				if( count < 15 ){
 					count++;
-					console.log( polygon, edge.start.x, edge.start.y, edge.end.x, edge.end.y, e, e % 4,
-									edge.startX, edge.startY, edge.endX, edge.endY);
+					console.log( polygon, edge.start.x, edge.start.y, edge.end.x, edge.end.y, e, e % 4 );
 				}
 				ctx.moveTo( edge.start.x, edge.start.y );
 				ctx.lineTo( edge.end.x, edge.end.y );
-				//ctx.moveTo( edge.startX, edge.startY );
-				//ctx.lineTo( edge.endX, edge.endY );
 				ctx.stroke();
 				e++;
 			}
