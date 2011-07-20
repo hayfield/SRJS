3a1dadd08aa9212498ac1e748439451286ccc9ec
hayfield <hayfield@gmx.com>
2011-07-20 12:22:40 +0100

    A Physics environment should have a canvas

--- js/src/physics/Environment.js
+++ js/src/physics/Environment.js
@@ -3,6 +3,21 @@ SRJS.Physics.Environment = function(){
 	this.polygons = new Array();
 	this.intersections = new Array();
 	
+	var canvas = document.createElement('canvas');
+	this.canvas = canvas;
+	this.canvas.width = SRJS.CURRENT_ARENA.renderer.domElement.width;
+	this.canvas.height = SRJS.CURRENT_ARENA.renderer.domElement.height;
+	document.body.appendChild( this.canvas );
+	this.context = this.canvas.getContext('2d');
+	
+	this.drawToCanvas = function(){
+		for( polygon in this.polygons ){
+			for( edge in polygon.edges ){
+				
+			}
+		}
+	};
+	
 };
 
 SRJS.Physics.Environment.prototype.addPolygon = function( polygon ){
