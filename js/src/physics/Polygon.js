eb351d279e5bad8bd1ccd533d0fc59fe859bc666
hayfield <hayfield@gmx.com>
2011-07-20 12:18:33 +0100

    Add a 'trigger' parameter

--- js/src/physics/Polygon.js
+++ js/src/physics/Polygon.js
@@ -1,7 +1,8 @@
-SRJS.Physics.Polygon = new function( fixed ){
+SRJS.Physics.Polygon = new function( fixed, trigger ){
 
 	this.edges = new Array();
 	this.fixed = fixed;
+	this.trigger = trigger;
 
 };
 
