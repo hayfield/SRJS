11c6b0db35968fefc952eca579163f2c2e2e7ceb
hayfield <hayfield@gmx.com>
2011-07-20 22:46:42 +0100

    Prevent some bits of code being run during prototype constructor calls. Fixes the WebGL view so it is able to display at the same time as the physics canvas (broke with commit 'Create the physics system from within the geometry calls')

--- js/src/arena/Wall.js
+++ js/src/arena/Wall.js
@@ -1,5 +1,5 @@
 SRJS.Wall = function( width, height, depth, position, rotation, material ){
-	
+if( arguments.length > 0 ){	// prevent the code being run on the constructor call from SRJS.Cube/Trigger
 	var geometry = new THREE.CubeGeometry( width, height, depth );
 	var materials = material || SRJS.Material.white;
 	
@@ -9,13 +9,14 @@ SRJS.Wall = function( width, height, depth, position, rotation, material ){
 	this.rotation = rotation instanceof THREE.Vector3 ? rotation : this.rotation;
 	//console.log(this, typeof this, typeof SRJS.Robot, typeof SRJS.Trigger);
 	//console.log('wall', width, height, depth, position, rotation, material);
+	console.log('phys',SRJS.phys, arguments.length);
 	SRJS.phys.addPolygon( new SRJS.Physics.Rectangle( !(this instanceof SRJS.Robot), this instanceof SRJS.Trigger,
 														new SRJS.Vector2( width, depth ),
 														new SRJS.Vector2( this.position.x, this.position.z ),
 														this.rotation.y ));
 	
 	//console.log(this);
-	
+}	
 };
 
 SRJS.Wall.prototype = new THREE.Mesh();
