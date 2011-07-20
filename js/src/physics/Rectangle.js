3af0ff710457a0b0a3367128c2e25c9fc28e138c
hayfield <hayfield@gmx.com>
2011-07-20 12:46:00 +0100

    Add a Rectangle Physics thing

---
+++ js/src/physics/Rectangle.js
@@ -0,0 +1,26 @@
+SRJS.Physics.Rectangle = new function( fixed, trigger, dimension, position ){
+	
+	SRJS.Physics.Polygon.call( this, fixed, trigger );
+	
+	var topLeft = new THREE.Vector2( position.x - (dimension.x / 2),
+									 position.y - (dimension.y / 2) );
+	var topRight = new THREE.Vector2( position.x + (dimension.x / 2),
+									  position.y - (dimension.y / 2) );
+	var bottomLeft = new THREE.Vector2( position.x - (dimension.x / 2),
+										position.y + (dimension.y / 2) );
+	var bottomRight = new THREE.Vector2( position.x + (dimension.x / 2),
+										 position.y + (dimension.y / 2) );
+	
+	// top
+	this.addEdge( new SRJS.Physics.Edge( topLeft, topRight ));
+	// bottom
+	this.addEdge( new SRJS.Physics.Edge( bottomLeft, bottomRight ));
+	// left
+	this.addEdge( new SRJS.Physics.Edge( topLeft, bottomLeft ));
+	// right
+	this.addEdge( new SRJS.Physics.Edge( topRight, bottomRight ));
+	
+};
+
+SRJS.Physics.Rectangle.prototype = new SRJS.Physics.Polygon();
+SRJS.Physics.Rectangle.prototype.constructor = SRJS.Physics.Rectangle;
