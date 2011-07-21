bc728af95dbf12d71ad9c92997566e8f7dd738d1
hayfield <hayfield@gmx.com>
2011-07-21 17:02:42 +0100

    Make sure onRobotEnter is always a function

--- js/src/arena/Trigger.js
+++ js/src/arena/Trigger.js
@@ -2,7 +2,7 @@ SRJS.Trigger = function( width, height, depth, position, rotation, action ){
 	
 	SRJS.Wall.call( this, width, height, depth, position, rotation, SRJS.Material.blue );
 	
-	this.onRobotEnter = action;
+	this.onRobotEnter = action || new function(){};
 
 };
 
