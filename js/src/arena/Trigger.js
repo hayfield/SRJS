84e0e3ae6ed6d4f9c1833b32737a757e8316124e
hayfield <hayfield@gmx.com>
2011-07-21 17:30:01 +0100

    Add Triggers to an array when they're created

--- js/src/arena/Trigger.js
+++ js/src/arena/Trigger.js
@@ -3,6 +3,8 @@ SRJS.Trigger = function( width, height, depth, position, rotation, action ){
 	SRJS.Wall.call( this, width, height, depth, position, rotation, SRJS.Material.blue );
 	
 	this.onRobotEnter = action || new function(){};
+	
+	SRJS.CURRENT_ARENA.triggers.push( this );
 
 };
 
