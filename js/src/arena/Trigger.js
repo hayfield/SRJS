d12a4d6766e4f3e38680e6c03f4e3a97616100e2
hayfield <hayfield@gmx.com>
2011-07-21 19:19:00 +0100

    Comment out console.log()s that were still printing things

--- js/src/arena/Trigger.js
+++ js/src/arena/Trigger.js
@@ -4,8 +4,8 @@ SRJS.Trigger = function( width, height, depth, position, rotation, action ){
 	
 	this.onRobotEnter = action || function( robotID ){ console.log('enter', robotID); };
 	//this.onRobotEnter = function( robotID ){ console.log('enter', robotID); };
-	this.onRobotStay = function( robotID ){ console.log('stay', robotID); };
-	this.onRobotExit = function( robotID ){ console.log('exit', robotID); };
+	this.onRobotStay = function( robotID ){ /*console.log('stay', robotID);*/ };
+	this.onRobotExit = function( robotID ){ /*console.log('exit', robotID);*/ };
 	
 	this.intersectingRobots = new Array();
 	this.previousIntersectingRobots = new Array();
