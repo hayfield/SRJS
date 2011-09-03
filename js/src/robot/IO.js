90b62090d86c0f523670d61ffc9a3c365d882b7d
hayfield <hayfield@gmx.com>
2011-09-03 10:14:01 +0100

    Create base for bumpSensor IO input

--- js/src/robot/IO.js
+++ js/src/robot/IO.js
@@ -2,6 +2,16 @@ SRJS.Robot.IO = function(){
 	
 	Array.call( this );
 	
+	this.input = function( ID ){
+		if( ID < bumpSensor.length ){
+			return bumpSensor[ID];
+		}
+	};
+	
+	this.bumpSensor = new Array();
+	
+	
+	
 };
 
 SRJS.Robot.IO.prototype = new Array();
