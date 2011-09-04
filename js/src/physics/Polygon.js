dada33753853e0c87cb379793d69b5fbf26f2329
hayfield <hayfield@gmx.com>
2011-09-04 13:32:54 +0100

    Prevent intersections being displayed between a robot and its own range finders

--- js/src/physics/Polygon.js
+++ js/src/physics/Polygon.js
@@ -66,9 +66,11 @@ SRJS.Physics.Polygon.prototype.intersectsWith = function( other ){
 			if( this.edges[e].intersects( other.edges[o] ) ){
 				if( this.object instanceof SRJS.Robot.BumpSensor ){
 					return true;
-				} else if( this.object instanceof SRJS.Robot.RangeFinder && other.object !== this.object.robot ){
-					this.object.ray.intersections.push( this.edges[e].intersects( other.edges[o] ), other.trigger );
-					intersects = true;
+				} else if( this.object instanceof SRJS.Robot.RangeFinder ){
+					if( other.object !== this.object.robot ){
+						this.object.ray.intersections.push( this.edges[e].intersects( other.edges[o] ), other.trigger );
+						intersects = true;
+					}
 				} else {
 					SRJS.intersections.push( this.edges[e].intersects( other.edges[o] ), other.trigger );
 					intersects = true;
