ee6ce57e5c394a9647575bd3fa47aa6511463da8
hayfield <hayfield@gmx.com>
2011-09-04 23:41:11 +0100

    Change materials from Phong to Lambert so that inconsistent lighting doesn't cause problems for the vision system

--- js/src/arena/Material.js
+++ js/src/arena/Material.js
@@ -1,9 +1,9 @@
 SRJS.Material = {
 	
-	white: new THREE.MeshPhongMaterial( { color: 0xffffff } ),
-	red: new THREE.MeshPhongMaterial( { color: 0xff0000 } ),
-	green: new THREE.MeshPhongMaterial( { color: 0x00ff00 } ),
-	blue: new THREE.MeshPhongMaterial( { color: 0x0000ff } ),
-	yellow: new THREE.MeshPhongMaterial( { color: 0xffff00 } ),
+	white: new THREE.MeshLambertMaterial( { color: 0xffffff } ),
+	red: new THREE.MeshLambertMaterial( { color: 0xff0000 } ),
+	green: new THREE.MeshLambertMaterial( { color: 0x00ff00 } ),
+	blue: new THREE.MeshLambertMaterial( { color: 0x0000ff } ),
+	yellow: new THREE.MeshLambertMaterial( { color: 0xffff00 } ),
 	
 };
