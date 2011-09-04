b450439b0fac3280432d87018b0745fe2b0f0318
hayfield <hayfield@gmx.com>
2011-09-04 13:21:29 +0100

    Remove some unneeded code

--- js/src/physics/Intersections.js
+++ js/src/physics/Intersections.js
@@ -7,22 +7,7 @@ SRJS.Physics.Intersections = function(){
 	
 	// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/sort
 	this.sortFunction = function( a, b ){
-		//var aDistance = a.subtract( SRJS.Physics.Intersections._sortPoint ).length();
-		//var bDistance = b.subtract( SRJS.Physics.Intersections._sortPoint ).length();
-		
 		return a.distanceTo( SRJS.Physics.Intersections._sortPoint ) - b.distanceTo( SRJS.Physics.Intersections._sortPoint );
-	};
-	
-	this.sort = function( arr ){
-		if( arr.length < 2 ){
-			return arr;
-		}
-		var pos;
-		pos = 0;
-		while( pos < arr.length ){
-			
-			pos++;
-		}
 	};
 	
 };
@@ -45,11 +30,12 @@ SRJS.Physics.Intersections.prototype.clear = function(){
 };
 
 SRJS.Physics.Intersections.prototype.nearestTo = function( point, includeSolids, includeTriggers ){
-	//this.sort(this.solids);
+
 	SRJS.Physics.Intersections._sortPoint = point;
 	if( !includeSolids && !includeTriggers ){
 		return false;
 	}
+	
 	var nearest = new Array();
 	if( includeSolids && this.solids.length > 0 ){
 		if( this.solids.length > 1 ){
@@ -63,6 +49,7 @@ SRJS.Physics.Intersections.prototype.nearestTo = function( point, includeSolids,
 		}
 		nearest.push( this.triggers[0] );
 	}
+	
 	if( nearest.length > 0 ){
 		nearest.sort( this.sortFunction );
 		return nearest[0];
