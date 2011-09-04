645dd0727dba58cc4209b304f63ad4ec319f6996
hayfield <hayfield@gmx.com>
2011-09-04 13:40:27 +0100

    Change number of ranger finders per robot to 4 so there are less lines on screen

--- js/src/SRJS.js
+++ js/src/SRJS.js
@@ -7,7 +7,7 @@ SRJS.physicsDimension = 800; // the dimension of the physics canvas
 SRJS.displayStats = true;
 SRJS.displayRobotVision = false;
 SRJS.bumpSensorsPerRobot = 16; // multiple of 4
-SRJS.rangeFindersPerRobot = 20; // multiple of 4
+SRJS.rangeFindersPerRobot = 4; // multiple of 4
 
 SRJS.isZero = function( value ){
 	return Math.abs( value ) < 0.00000001;
