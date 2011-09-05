df24667de3c7815bf2b36dd69866be57ac5139bd
hayfield <hayfield@gmx.com>
2011-09-05 10:07:39 +0100

    Add a method of getting the analogue value for the range finder

--- js/src/robot/RangeFinder.js
+++ js/src/robot/RangeFinder.js
@@ -40,3 +40,17 @@ SRJS.Robot.RangeFinder = function( parentRobot, ID ){
 	SRJS.phys.addRangeFinder( this );
 	
 };
+
+SRJS.Robot.RangeFinder.prototype.__defineGetter__('a',
+	function(){
+		var raw = this.ray.edges[0].start.distanceTo( this.ray.nearestIntersection );
+		var value = 0;
+		if( raw > 0 ){
+			value = 100 / raw;
+			value = value > 3.3 ? 3.3 : value;
+		} else if( raw === 0 ){
+			value = 3.3;
+		}
+		return value;
+	}
+);
