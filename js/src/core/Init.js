28b79a19256ab4e511122328b1504b17d8a3840f
hayfield <hayfield@gmx.com>
2011-09-09 11:46:03 +0100

    Add an info message if WebGL isn't supported.

--- js/src/core/Init.js
+++ js/src/core/Init.js
@@ -1,5 +1,6 @@
 SRJS.Init = function(){
 	
+	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 	SRJS.intersections = new SRJS.Physics.Intersections();
 	SRJS.phys = new SRJS.Physics.Environment();
 	var bob = new SRJS.Arena2011();
