a76b8cc31da8e6a1884f2de3b89022a2a603c547
hayfield <hayfield@gmx.com>
2011-09-16 16:11:42 +0100

    Add getters to walls to work out how high the base and top are

--- js/src/arena/Wall.js
+++ js/src/arena/Wall.js
@@ -3,6 +3,10 @@ if( arguments.length > 0 ){	// prevent the code being run on the constructor cal
 	var geometry = new THREE.CubeGeometry( width, height, depth ),
 		materials = material || SRJS.Material.white;
 	
+	this.wallWidth = width;
+	this.wallHeight = height;
+	this.wallDepth = depth;
+	
 	THREE.Mesh.call( this, geometry, materials );
 	
 	this.position = position instanceof THREE.Vector3 ? position : this.position;
@@ -13,6 +17,21 @@ if( arguments.length > 0 ){	// prevent the code being run on the constructor cal
 														new SRJS.Vector2( this.position.x, this.position.z ),
 														this.rotation.y, this ));
 
+	this._heightOfBaseGetter = function(){
+		return this.position.y - this.wallHeight / 2;
+	};
+	
+	Object.defineProperty(this, 'heightOfBase', {
+		get: this._heightOfBaseGetter
+	});
+	
+	this._heightOfTopGetter = function(){
+		return this.position.y + this.wallHeight / 2;
+	};
+	
+	Object.defineProperty(this, 'heightOfTop', {
+		get: this._heightOfTopGetter
+	});
 }	
 };
 
