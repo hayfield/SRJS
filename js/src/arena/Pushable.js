90ce3f6ad832b92fad81adff8d6cb4efd7797a96
hayfield <hayfield@gmx.com>
2011-09-16 16:59:57 +0100

    Make pushable objects black in the 3D scene

--- js/src/arena/Pushable.js
+++ js/src/arena/Pushable.js
@@ -1,6 +1,6 @@
 SRJS.Pushable = function( width, height, depth, position, rotation ){
 	
-	SRJS.Wall.call( this, width, height, depth, position, rotation, SRJS.Material.red );
+	SRJS.Wall.call( this, width, height, depth, position, rotation, SRJS.Material.black );
 
 };
 
