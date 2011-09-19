517a1536cd949aeba7895842461a639226046ffd
hayfield <hayfield@gmx.com>
2011-09-19 10:27:07 +0100

    Give VisionV2 a canvas so it's possible to see where the robot's heading

--- js/src/vision/VisionV2.js
+++ js/src/vision/VisionV2.js
@@ -2,8 +2,23 @@ SRJS.VisionV2 = function( object ){
 	
 	this.object = object;
 	
-	this.update = function(){
+	var canvas = document.createElement('canvas'),
+		vision = this;
+	
+	this.canvas = canvas;
+	this.canvas.width = SRJS.CURRENT_ARENA.renderer.domElement.width;
+	this.canvas.height = SRJS.CURRENT_ARENA.renderer.domElement.height;
+	document.body.appendChild( this.canvas );
+	this.context = this.canvas.getContext('2d');
+	
+	this.update = function( renderer ){
+		var img = new Image();
 		
+		img.onload = function(){
+			vision.context.clearRect( 0, 0, vision.canvas.width, vision.canvas.height );
+			vision.context.drawImage( img, 0, 0 );
+		};
+		img.src = renderer.domElement.toDataURL('image/png');
 	};
 	
 	this.print_marker = function( marker ){
