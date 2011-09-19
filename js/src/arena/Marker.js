8f95673239662bd606606452af1b8ceb152e1e45
hayfield <hayfield@gmx.com>
2011-09-19 11:54:30 +0100

    Pass a number rather than a THREE.Vector3 to Math.sin() and Math.cos() so that the bearing has a value rather than being NaN

--- js/src/arena/Marker.js
+++ js/src/arena/Marker.js
@@ -24,7 +24,7 @@ SRJS.Marker = function( parentObject ){
 		if( direction.x === 0 && direction.y === 0 ){
 			return new SRJS.Vector2( 0, 0 );
 		}
-		var up = new SRJS.Vector2( Math.sin(this.object.rotation), Math.cos(this.object.rotation) ),
+		var up = new SRJS.Vector2( Math.sin(this.object.rotation.y), Math.cos(this.object.rotation.y) ),
 			angle = up.angleTo( direction );
 		
 		if( direction.x < 0 ){
