57b142e9de8ec97b5a2ab24732129639930b0057
hayfield <hayfield@gmx.com>
2011-09-19 09:59:57 +0100

    Change the default arena to 2012.
    A parameter can be passed to SRJS.Init() to select other arenas.

--- js/src/core/Init.js
+++ js/src/core/Init.js
@@ -1,8 +1,13 @@
-SRJS.Init = function(){
+SRJS.Init = function( year ){
 	
 	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 	SRJS.intersections = new SRJS.Physics.Intersections();
 	SRJS.phys = new SRJS.Physics.Environment();
-	var bob = new SRJS.Arena2011();
+	
+	if( year === 2011 ){
+		var bob = new SRJS.Arena2011();
+	} else {
+		var bob = new SRJS.Arena2012();
+	}
 	
 };
