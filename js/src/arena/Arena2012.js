d59baafa267f93f0c487434b9e722f8d8609799b
hayfield <hayfield@gmx.com>
2011-09-19 09:55:11 +0100

    Create the basis for Arena2012

---
+++ js/src/arena/Arena2012.js
@@ -0,0 +1,63 @@
+SRJS.Arena2012 = function(){
+	
+	var args = {}, scene;
+
+	args.initScene = function(){
+		
+        scene = new THREE.Scene();
+		
+		var i, ambient, directionalLight, pointLight;
+		
+		ambient = new THREE.AmbientLight( 0xffffff );
+		scene.addLight( ambient );
+ 
+		directionalLight = new THREE.DirectionalLight( 0xff0000 );
+		directionalLight.position.y = 200;
+		directionalLight.position.normalize();
+		scene.addLight( directionalLight );
+ 
+		pointLight = new THREE.PointLight( 0xff0000 );
+		pointLight.position.y = 150;
+		pointLight.position.x = -250;
+		pointLight.position.z = -250;
+		pointLight.intensity = 0.5;
+		scene.addLight( pointLight );
+
+		// floor
+		scene.addObject( new SRJS.Wall( 800, 100, 800 ) );
+
+		// 4 outer walls
+		scene.addObject( new SRJS.Wall( 100, 60, 1000, new THREE.Vector3( 450, 80, 0 ) ) );
+		scene.addObject( new SRJS.Wall( 100, 60, 1000, new THREE.Vector3( -450, 80, 0 ) ) );
+		scene.addObject( new SRJS.Wall( 100, 60, 1000,
+										new THREE.Vector3( 0, 80, -450 ),
+										new THREE.Vector3( 0, Math.PI / 2, 0 )
+									) );
+		scene.addObject( new SRJS.Wall( 100, 60, 1000,
+										new THREE.Vector3( 0, 80, 450 ),
+										new THREE.Vector3( 0, Math.PI / 2, 0 )
+									) );
+		
+		
+
+		this.scene = scene;
+	};
+	
+	args.physics = SRJS.phys;
+	
+	args.arenaDimension = 800;
+	
+	args.robotStartPositions = [
+									new SRJS.Vector2( 365, 365 ),
+									new SRJS.Vector2( 365, -365 ),
+									new SRJS.Vector2( -365, -365 ),
+									new SRJS.Vector2( -365, 365 )
+							   ];
+	
+	args.robotStartRotations = [ 0, Math.PI / 2, Math.PI, Math.PI * 1.5 ];
+	
+	args.visionVersion = 1;
+	
+	return new SRJS.Arena( args );
+	
+};
