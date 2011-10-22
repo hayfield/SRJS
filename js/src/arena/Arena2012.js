e76d457dc21086853ee900590cc365b18bf96b01
hayfield <hayfield@gmx.com>
2011-10-22 11:37:03 +0100

    Change the bucket dimensions to match the rulebook

--- js/src/arena/Arena2012.js
+++ js/src/arena/Arena2012.js
@@ -140,7 +140,7 @@ SRJS.Arena2012 = function(){
 			new THREE.Vector3( -350, 100, 0 )
 		];
 		for( i = 0; i < 4; i++ ){
-			bucket = new SRJS.Pushable( 50, 100, 50,
+			bucket = new SRJS.Pushable( 24.5, 100, 37.2,
 										bucketPositions[i],
 										new THREE.Vector3( 0, 0, 0 )
 									);
